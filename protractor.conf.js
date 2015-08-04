a6d00e346e534458fdd886b62960440a3d947f8e
netanel basal <netanel7799@gmail.com>
2015-08-04 19:36:06 +0300

    4444

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,8 +1,15 @@
+var phantomjs = require('phantomjs');
+
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['specs/spec.js'],
   capabilities: {
-    browserName: 'chrome'
+    'browserName': 'phantomjs'
+  },
+  phantomjs: {
+    binary: {
+      path: phantomjs.path
+    }
   },
   jasmineNodeOpts: {
     showColors: true, // Use colors in the command line report.
